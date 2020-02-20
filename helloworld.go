1d0c6c318894d4541ec82356e8d106b80384196e
Lindows <sangeoul@naver.com>
2020-02-20 22:56:12 +0900

    master branch commit

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,9 @@
 package main
+
 import "fmt"
 
-func main(){
-    fmt.Println("Hello World")
-    fmt.Println("is GIT OK?")
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("is GIT OK?")
+	fmt.Println("this is master Branch")
 }
